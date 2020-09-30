e30da2a7fa7415ba541c7aad910bdcdfe4960aca
Yan Kisen <yan.kisen@coresecure.com>
2020-09-30 11:31:25 -0400

    initial GH Action setup

---
+++ @types/contentful.d.ts
@@ -0,0 +1,47 @@
+import {Entry, SyncCollection} from 'contentful'
+
+export interface IChangelistFields {
+  /** Title (UI only) */
+  entryTitle: string
+
+  /** Changelist ID */
+  changelistId: string
+
+  /** Feature Name (Branch) */
+  featureName: string
+
+  /** Approved By Legal */
+  approvedByLegal: 'Yes'[]
+
+  /** Review State  */
+  reviewState:
+    | 'Initial authoring'
+    | 'Needs Review'
+    | 'Needs changes'
+    | 'Ready to publish'
+
+  /** Changelist Entries */
+  entries?: Entry<{[fieldId: string]: unknown}>[] | undefined
+}
+
+/** A collection of content updates to be included in a deployment. */
+
+export interface IChangelist extends Entry<IChangelistFields> {
+  sys: {
+    id: string
+    type: string
+    createdAt: string
+    updatedAt: string
+    locale: string
+    contentType: {
+      sys: {
+        id: 'changelist'
+        linkType: 'ContentType'
+        type: 'Link'
+      }
+    }
+  }
+}
+
+
+export type LocalSyncCollection = Pick<SyncCollection, 'entries' | 'assets' | 'deletedEntries' | 'deletedAssets' | 'nextSyncToken'>; // ## preserve only relevant properties
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,107 @@
+import {createClient, Entry, SyncCollection} from 'contentful'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {IChangelist, LocalSyncCollection} from '../@types/contentful'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+      const changelistId: string = core.getInput('ctf-query')
+      core.debug(`ctf-changelist-sync | arbitrary debug message`) // NOTE: debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+      core.info(`ctf-changelist-sync | changelist: [${changelistId}]`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+      let resultCollection: LocalSyncCollection
+      // ## Configuration variables
 
-    core.setOutput('time', new Date().toTimeString())
+      const CTF_SPACE_ID = process.env.CTF_SPACE_ID
+      const CTF_CDA_ACCESS_TOKEN = process.env.CTF_CDA_ACCESS_TOKEN
+      const CTF_CPA_ACCESS_TOKEN = process.env.CTF_CPA_ACCESS_TOKEN // NOTE: Content Delivery API vs Content Preview API
+      const CTF_ENVIRONMENT_ID = process.env.CTF_ENVIRONMENT_ID || 'master'
+      if (!CTF_SPACE_ID || !CTF_CDA_ACCESS_TOKEN || !CTF_CPA_ACCESS_TOKEN) {
+          core.setFailed('Invalid Contentful Client config')
+          return
+      }
+
+      const ctfClient = createClient({
+          space: CTF_SPACE_ID,
+          accessToken: CTF_CDA_ACCESS_TOKEN,
+          environment: CTF_ENVIRONMENT_ID,
+          resolveLinks: false // NOTE: we don't want to resolve the object graph
+      })
+
+      // (1) Perform initial Sync
+      const syncResult = await ctfClient.sync({
+          type: 'all',
+          initial: true
+      })
+      core.info(`executeSyncInit: [${syncResult.nextSyncToken}]`)
+
+      // (2) If supplied, query by a changelist
+      if (!changelistId) {
+          resultCollection = syncResult;
+      } else {
+          const ctfPreviewClient = createClient({
+              space: CTF_SPACE_ID,
+              accessToken: CTF_CPA_ACCESS_TOKEN,
+              environment: CTF_ENVIRONMENT_ID,
+              host: 'preview.contentful.com',
+              resolveLinks: false // NOTE: we don't want to resolve the object graph
+          })
+          const changelist: IChangelist = (
+              await ctfPreviewClient.getEntries({
+                  content_type: 'changelist',
+                  'fields.changelistid': changelistId,
+                  select: 'sys.id,fields.entries',
+                  include: 1
+              })
+          ).items[0] as IChangelist
+
+          core.info(
+              `Changelist [${changelistId}] contains ${changelist.fields.entries?.length} entries`
+          )
+
+          // (3) Merge the JSON Data
+          // TODO: I am sure there is a more efficient way of achieving this...
+          // TODO/NOTE: For now we *only* deal with Entries, not Assets. However, convention implies that Assets would be wrapped by a "Media" ContentType
+
+          const updatedEntries = changelist.fields.entries?.filter(item => {
+              return syncResult.entries.filter(childItem => {
+                  item.sys.id === childItem.sys.id
+              })
+          }) as Entry<any>[]
+          const newEntries = changelist.fields.entries?.filter(item => {
+              return syncResult.entries.filter(childItem => {
+                  item.sys.id !== childItem.sys.id
+              })
+          }) as Entry<any>[]
+
+          const updatedEntryIds = updatedEntries.map(item => item.sys.id)
+
+          // (4.1) Modify Sync Collection
+           resultCollection = {
+              entries: syncResult.entries.map(originalEntry => {
+                  if (updatedEntryIds.includes(originalEntry.sys.id)) {
+                      return updatedEntries.find(
+                          item => item.sys.id === originalEntry.sys.id
+                      ) as Entry<any>
+                  }
+                  return originalEntry
+              }),
+              assets: syncResult.assets,
+
+              deletedAssets: syncResult.deletedAssets,
+              deletedEntries: syncResult.deletedEntries,
+              nextSyncToken: syncResult.nextSyncToken
+          }
+
+          // (4.2) Concatenate *NEW* Entries to the Collection
+          resultCollection.entries.concat(newEntries)
+      }
+
+
+  }
+    core.setOutput(
+      'something',
+      `There's always gotta be something.... ${new Date().toTimeString()}`
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
